7a5db36989114ead8eb55c16e97ade9df63fc287
Ali Moghimi <mghmiali@gmail.com>
2025-09-01 22:05:39 +0000

    fix: include new abstract methods

--- repositories/memory_repo.py
+++ repositories/memory_repo.py
@@ -1,10 +1,10 @@
 from typing import Dict, Iterable, Optional
 
-from domain.models import Task, TaskId
+from entities.models import Task, TaskId
+from interfaces.repository_interface import TaskRepository
 
 
-class InMemoryTaskRepo:
-
+class InMemoryTaskRepo(TaskRepository):
     def __init__(self):
         self._db: Dict[str, Task] = {}
 
@@ -19,3 +19,9 @@ class InMemoryTaskRepo:
 
     def save(self, task: Task) -> None:
         self._db[task.id.value] = task
+
+    def delete(self, id: TaskId) -> None:
+        self._db.pop(id.value, None)
+
+    def close(self) -> None:
+        pass
